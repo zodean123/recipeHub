8d997ee1371db6caee473bb813e11bfa495ccf2f
Dipan Kumar Mallick <110153417+zodean123@users.noreply.github.com>
2024-09-18 18:15:33 +0530

    Update allowedOrigins.js

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
   "https://recipen.vercel.app",
   "https://recipen-backend.onrender.com",
+  "https://recipe-hub-vert.vercel.app/",
   `${process.env.CLIENT_BASE_URL}`
 ];
 

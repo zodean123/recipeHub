0331ca3bb12327f457b96edfd194c8dbb4724215
zodean123 <dipanmallick7085@gmail.com>
2024-09-18 18:59:42 +0530

    udated credentials

--- server/middleware/credentials.js
+++ server/middleware/credentials.js
@@ -1,10 +1,6 @@
-const allowedOrigins = require("../config/allowedOrigins");
-
 const credentials = (req, res, next) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.header("Access-Control-Allow-Credentials", true);
-  }
+  // Always set the Access-Control-Allow-Credentials header
+  res.header("Access-Control-Allow-Credentials", true);
   next();
 };
 
